e1a3f7f7f4b81d9390986cd2cc4ee91aaa9607e9
KhaledAmgad <khaled.mohamed98@eng-st.cu.edu.eg>
2019-03-22 20:47:17 +0200

    Code Styling Es-lint Notifications

--- Kwikker/App/Components/Notification/Styles.js
+++ Kwikker/App/Components/Notification/Styles.js
@@ -1,36 +1,38 @@
 import { StyleSheet } from 'react-native';
 
+
+// eslint-disable-next-line no-undef
 export default styles = StyleSheet.create({
-    notificationContainer : {
-        flex: 1,    
-        height: 150,
-        borderColor:"lightgray",
-        borderWidth: 0.5,
-    },
-    profilePicture : {
-        height:50,
-        width: 50,
-        borderRadius: 50/2,
-        borderColor: 'lightgray',
-        borderWidth: 1,
-        overflow: 'hidden',
-        marginTop: 10,
-        marginLeft: 40,
-    },
-    ProfileImage:{
-        flex:1, width:null,height:null
-    },
-    textContainer : {
-        flex: 1,   
-        marginLeft: 40,
-    },
-    textHeader:{
-        flex: 1, 
-        flexDirection: 'row' ,
-        marginTop: 10,
-    },
-    textContent:{
-        marginBottom:40 ,
-        color:'lightgray'
-    },
-  });
+  notificationContainer: {
+    flex: 1,
+    height: 150,
+    borderColor: 'lightgray',
+    borderWidth: 0.5,
+  },
+  profilePicture: {
+    height: 50,
+    width: 50,
+    borderRadius: 50 / 2,
+    borderColor: 'lightgray',
+    borderWidth: 1,
+    overflow: 'hidden',
+    marginTop: 10,
+    marginLeft: 40,
+  },
+  ProfileImage: {
+    flex: 1, width: null, height: null
+  },
+  textContainer: {
+    flex: 1,
+    marginLeft: 40,
+  },
+  textHeader: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  textContent: {
+    marginBottom: 40,
+    color: 'lightgray'
+  },
+});
--- Kwikker/App/Screens/Notifications/Notifications.js
+++ Kwikker/App/Screens/Notifications/Notifications.js
@@ -1,107 +1,100 @@
 import React, { Component } from 'react';
-import { ScrollView,RefreshControl } from 'react-native';
+import { ScrollView, RefreshControl } from 'react-native';
 import axios from 'axios';
-import Notification from '../../Components/Notification/Notification'
+import Notification from '../../Components/Notification/Notification';
 
-export default class Notifications extends Component
-{
+export default class Notifications extends Component {
   constructor(props) {
     super(props);
-    this.state=
-    {
-      notifications:[],
+    this.state = {
+      notifications: [],
       refreshing: false
-    }
+    };
   }
-  
-/** Get more Notifications when we get to the end of the scrollView.
- * Check we reached end of content 
+
+  componentDidMount() {
+    this.updateNotifications();
+  }
+
+  pullRefresh= () => {
+    this.setState({
+      refreshing: true,
+      notifications: []
+    });
+    this.updateNotifications();
+    this.setState({
+      refreshing: false
+    });
+  }
+
+  /** Get more Notifications when we get to the end of the scrollView.
+ * Check we reached end of content
  * @param {int} layoutMeasurement - size of the layout .
  * @param  {int} contentOffset - position on screen
  * @param  {int} contentSize - size of all content
  */
-  MoreNotifications=({layoutMeasurement,contentOffset,contentSize })=>
-  {
-      if(layoutMeasurement.height + contentOffset.y >= contentSize.height)
-      {
-        this.updateNotifications(this.state.notifications.length-1)
-      }
+  MoreNotifications=({ layoutMeasurement, contentOffset, contentSize }) => {
+    if (layoutMeasurement.height + contentOffset.y >= contentSize.height) {
+      this.updateNotifications(this.state.notifications.length - 1);
+    }
   }
 
-/** Update Notifications.
+  /** Update Notifications.
  * gets first 20 Notification with defult With default parameter (id=null)
  * To retrieve more send the id of the last retrieved notification.
  * @param {int} id - The id of Notification .
  */
-  updateNotifications(id=null)
-  {
+  updateNotifications(id = null) {
     axios.get('/notifications', {
       params: {
-        id: id
+        id
       }
     })
-    .then((response) => {
-      this.setState({
-        notifications: this.state.notifications.concat(response.data) 
-        })
-      console.log(response.data);
-    })
-    .catch(function (error) {
+
+      .then((response) => {
+        this.setState({
+          notifications: this.setState((prevState) => ({ notifications: prevState.notifications.concat(response.data) }))
+        });
+        // console.log(response.data);
+      })
+      .catch((error) => {
       // handle error
-      console.log(error);
-    })
-    .then(function () {
+        // console.log(error);
+      })
+      .then(() => {
       // always executed
-    });
+      });
   }
+
   /** pull to refresh functionality.
    * gets first 20 notifications
   */
- pullRefresh= () =>
-  {
-    this.setState({
-      refreshing:true,
-      notifications: []
-      });
-    this.updateNotifications();
-    this.setState({
-      refreshing:false
-      });
-  }
 
 
-
-  
-
-
-  componentDidMount()
-  {
-    this.updateNotifications();
-  }
   render() {
-    
     return (
-        <ScrollView 
-        refreshControl ={
-        <RefreshControl
-          refreshing={this.state.refreshing}
-          onRefresh={this.pullRefresh}
-        />}
-        style={{ flex:1 }}
-        onScroll={ ({nativeEvent})=>{this.MoreNotifications(nativeEvent)}}> 
-        {this.state.notifications.map((item,index) => (
-          <Notification 
-          profileUrl = {item.profile_pic_URL} 
-          kweekText={item.kweek_text} 
-          type={item.type} 
-          screenName ={item.screen_name} 
-          ></Notification>
-          ))
+      <ScrollView
+        refreshControl={(
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this.pullRefresh}
+          />
+)}
+        style={{ flex: 1 }}
+        onScroll={({ nativeEvent }) => { this.MoreNotifications(nativeEvent); }}
+      >
+        {this.state.notifications.map((item, index) => (
+          <Notification
+            profileUrl={item.profile_pic_URL}
+            kweekText={item.kweek_text}
+            type={item.type}
+            screenName={item.screen_name}
+          />
+        ))
         }
-        
 
-        </ScrollView>
+
+      </ScrollView>
     );
   }
 }
-
