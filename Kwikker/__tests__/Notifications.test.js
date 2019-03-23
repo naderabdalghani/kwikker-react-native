73a4728fb5c645c299f782c5db0a464b90d24ded
KhaledAmgad <khaled.mohamed98@eng-st.cu.edu.eg>
2019-03-24 01:44:08 +0200

    Doc and Unit Testing

--- Kwikker/__tests__/Notifications.test.js
+++ Kwikker/__tests__/Notifications.test.js
@@ -6,15 +6,30 @@ import mockAxios from 'axios';
 import Notifications from '../App/Screens/Notifications/Notifications';
 
 describe('notificationsComponent component', () => {
-  it('componentDidMount: should turn on  loading , get 20 notifications ', async () => {
+  it('Mount:  get 20 notifications on component Mount ', async () => {
     const wrapper = shallow(<Notifications />);
     const Instance = wrapper.instance();
-    const RefreshInstance = await Instance.pullRefresh();
+    expect(wrapper.instance().state.refreshing).toBe(true);
+    expect(mockAxios.get).toHaveBeenCalledTimes(1);
+    expect(mockAxios.get).toHaveBeenCalledWith('notifications', {
+      params: {
+        last_notification_retrieved_id: null
+      }
+    });
+    const pullInstance = await Instance.pullRefresh();
     expect(wrapper.instance().state.refreshing).toBe(true);
     expect(mockAxios.get).toHaveBeenCalledTimes(2);
     expect(mockAxios.get).toHaveBeenCalledWith('notifications', {
       params: {
         last_notification_retrieved_id: null
+      }
+    });
+    const updateInstance = await Instance.updateNotifications(5);
+    expect(wrapper.instance().state.refreshing).toBe(true);
+    expect(mockAxios.get).toHaveBeenCalledTimes(3);
+    expect(mockAxios.get).toHaveBeenCalledWith('notifications', {
+      params: {
+        last_notification_retrieved_id: 5
       }
     });
   });
