22fc233478fa7c14f966ae7eeecd44229c9a4a35
Shady Fahmy <shadyfahmy67@gmail.com>
2019-04-13 09:05:18 +0200

    like - rekweek

--- Kwikker/App/Components/Kweek/Kweek.js
+++ Kwikker/App/Components/Kweek/Kweek.js
@@ -2,53 +2,79 @@ import React, { Component } from 'react';
 import { Thumbnail } from 'native-base';
 import { Text, View, TouchableOpacity, Image, Button, StyleSheet, TextInput } from 'react-native';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+//import console = require('console');
 
 export default class Kweek extends Component {
   constructor(props) {
     super(props);
   }
 
+  state = { liked: true, rekeeked: true, likesCounter: 17, rekweeksCounter: 18 };
+
   likeStatus() {
-    if (true) {
+    if (this.state.liked) {
       return (
-        <TouchableOpacity style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-          <AntDesign name="heart" size={16} color="#FF0000" />
-          <Text style={{ marginLeft: '3%' }}>18</Text>
+        <TouchableOpacity onPress={this.likePressed.bind(this)} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+          <Ionicons name="ios-heart" size={18} color="#FF0000" />
+          <Text style={{ marginLeft: '3%' }}>{this.state.likesCounter}</Text>
         </TouchableOpacity>
       );
     }
     return (
-      <TouchableOpacity style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-        <EvilIcons name="heart" size={28} color="#657786" />
-        <Text>18</Text>
+      <TouchableOpacity onPress={this.likePressed.bind(this)} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <Ionicons name="ios-heart-empty" size={18} color="#657786" />
+        <Text style={{ marginLeft: '3%' }}>{this.state.likesCounter}</Text>
       </TouchableOpacity>
     );
   }
 
   rekweekStatus() {
-    if (true) {
+    if (this.state.rekeeked) {
       return (
-        <TouchableOpacity style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={this.rekweekPressed.bind(this)} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
           <EvilIcons name="retweet" size={28} color="#009900" />
-          <Text>13</Text>
+          <Text>{this.state.rekweeksCounter}</Text>
         </TouchableOpacity>
       );
     }
     return (
-      <TouchableOpacity style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={this.rekweekPressed.bind(this)} style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
         <EvilIcons name="retweet" size={28} color="#657786" />
-        <Text>13</Text>
+        <Text>{this.state.rekweeksCounter}</Text>
       </TouchableOpacity>
     );
   }
 
+  likePressed() {
+    console.log('like pressed');
+    if (this.state.liked) {
+      this.setState({ liked: false });
+      this.setState((prevState) => ({ likesCounter: prevState.likesCounter - 1 }));
+    } else {
+      this.setState({ liked: true });
+      this.setState((prevState) => ({ likesCounter: prevState.likesCounter + 1 }));
+    }
+  }
+
+  rekweekPressed() {
+    console.log('rekweek pressed');
+    if (this.state.rekeeked) {
+      this.setState({ rekeeked: false });
+      this.setState((prevState) => ({ rekweeksCounter: prevState.rekweeksCounter - 1 }));
+    } else {
+      this.setState({ rekeeked: true });
+      this.setState((prevState) => ({ rekweeksCounter: prevState.rekweeksCounter + 1 }));
+    }
+  }
+
   render() {
+    console.log('inside home');
     return (
       <View style={{ borderBottomWidth: 0.5, borderBottomColor: '#AAB8C2' }}>
         <TouchableOpacity style={{ marginTop: '2%', marginBottom: '1%' }}>
           <View style={{ flexDirection: 'row' }}>
-            <AntDesign name="heart" size={11} color="#657786" style={{ marginLeft: '15%', marginTop: '1%' }} />
+            <Ionicons name="ios-heart" size={11} color="#657786" style={{ marginLeft: '15%', marginTop: '1%' }} />
             <Text style={{ color: '#657786', marginLeft: '4.5%' }}>user liked</Text>
           </View>
         </TouchableOpacity>
