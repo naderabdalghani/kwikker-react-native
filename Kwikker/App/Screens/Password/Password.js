14f1fd90a0dbd90a2821642d9d7a6aaf8b11812a
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2019-04-15 05:57:34 +0200

    Reset password via email link

--- Kwikker/App/Screens/Password/Password.js
+++ Kwikker/App/Screens/Password/Password.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Text, View, Image, TouchableOpacity, ScrollView, ToastAndroid, TouchableNativeFeedback } from 'react-native';
+import { Text, View, Image, TouchableOpacity, ScrollView, ToastAndroid, TouchableNativeFeedback, BackHandler } from 'react-native';
 import axios from 'axios';
 import CustomTextInput from '../../Components/CustomTextInput/CustomTextInput';
 import styles from './Styles';
 import CustomButton from '../../Components/CustomButton/CustomButton';
 
-
 export default class Password extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { New: '', Confirm: '', error: '', disable: false };
+    const boolVar = this.props.navigation.getParam('forgotPassword');
+    this.state = { New: '', Confirm: '', error: '', disable: false, forgotPasswordForm: boolVar };
   }
 
+  componentWillMount() {
+    if (this.state.forgotPasswordForm === true) {
+      BackHandler.addEventListener('hardwareBackPress', () => { return true; });
+    }
+  }
 
   /**
    * update user's password and go back to account settings if the new and confirm password are match and not empty, else Shows a toast message
@@ -29,12 +34,40 @@ export default class Password extends React.Component {
         })
         .then(() => {
           // always executed
-          this.props.navigation.goBack(null);
+          // //// Nader //////
+          if (this.state.forgotPasswordForm === true) {
+            axios.defaults.headers.common['TOKEN'] = '';
+            this.props.navigation.navigate('Login');
+          } else {
+            this.props.navigation.goBack(null);
+          }
         });
     } else {
       this.setState({ error: "passwords don't match or empty" });
       ToastAndroid.show(this.state.error, ToastAndroid.SHORT);
     }
+  }
+
+  backButtonRenderer() {
+    if (this.state.forgotPasswordForm === true) {
+      return (
+        <View style={styles.backButtonContainer}>
+          <TouchableNativeFeedback>
+            <View style={styles.backButton} />
+          </TouchableNativeFeedback>
+        </View>
+      );
+    }
+    return (
+      <View style={styles.backButtonContainer}>
+        <TouchableNativeFeedback onPress={() => this.props.navigation.goBack(null)}>
+          <Image
+            style={styles.backButton}
+            source={require('./../../Assets/Images/back_button.png')}
+          />
+        </TouchableNativeFeedback>
+      </View>
+    );
   }
 
   render() {
@@ -48,14 +81,7 @@ export default class Password extends React.Component {
             <View style={styles.upperPart}>
 
               <View style={styles.header}>
-                <View style={styles.backButtonContainer}>
-                  <TouchableNativeFeedback onPress={() => this.props.navigation.goBack(null)}>
-                    <Image
-                      style={styles.backButton}
-                      source={require('./../../Assets/Images/back_button.png')}
-                    />
-                  </TouchableNativeFeedback>
-                </View>
+                {this.backButtonRenderer()}
                 <View>
                   <Text style={styles.title}>Update password</Text>
                 </View>
