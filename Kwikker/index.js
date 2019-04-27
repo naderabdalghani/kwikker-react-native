2450cfa542e22a24ebf798a633a1d92a503af35c
KhaledAmgad <khaled.mohamed98@eng-st.cu.edu.eg>
2019-04-27 23:04:57 +0200

    Live Messages

--- Kwikker/index.js
+++ Kwikker/index.js
@@ -1,11 +1,18 @@
-import { AppRegistry, ToastAndroid } from 'react-native';
+import { AppRegistry, ToastAndroid, YellowBox } from 'react-native';
 import axios from 'axios';
 import App from './App';
 import { name as appName } from './app.json';
 
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillUpdate is deprecated',
+  'Setting a timer for a long period',
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Module RCTImageLoader requires',
+]);
 // 3000 is your own server port, it could be a different number
 // therefore copy the link json-server outputs once you run json-server --watch db.json
-
 // axios.defaults.baseURL = 'http://localhost:3000'; // fake json-server
 axios.defaults.baseURL = 'http://kwikkerbackend.eu-central-1.elasticbeanstalk.com/'; // actual server
 axios.defaults.timeout = 5000;
