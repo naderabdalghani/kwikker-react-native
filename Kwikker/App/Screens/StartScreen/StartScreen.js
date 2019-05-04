a95acda5c063211945f8678ccadf04003d996340
Nader AbdAlGhani <nader_abdelghani@hotmail.com>
2019-05-04 19:39:15 +0200

    Fix Sign Out Navigation

--- Kwikker/App/Screens/StartScreen/StartScreen.js
+++ Kwikker/App/Screens/StartScreen/StartScreen.js
@@ -22,10 +22,13 @@ export default class StartScreen extends Component {
     this.setState({ blank: false });
     if (Platform.OS === 'android') {
       Linking.getInitialURL().then((url) => {
-        if (url.includes('kwikker.me/confirm')) {
-          this.confirmUser(url);
-        } else if (url.includes('kwikker.me/reset_password')) {
-          this.resetPassword(url);
+        if (url && global.deepLinking) {
+          global.deepLinking = false;
+          if (url.includes('kwikker.me/confirm')) {
+            this.confirmUser(url);
+          } else if (url.includes('kwikker.me/reset_password')) {
+            this.resetPassword(url);
+          }
         }
       })
         .catch((err) => {
@@ -37,6 +40,13 @@ export default class StartScreen extends Component {
 
   componentWillUnmount() {
     Linking.removeEventListener('url', this.handleOpenURL);
+    Linking.getInitialURL().then((url) => {
+      console.log('url before:');
+      console.log(url);
+      url = '';
+      console.log('url after:');
+      console.log(url);
+    }).catch(() => {});
   }
 
   /**
